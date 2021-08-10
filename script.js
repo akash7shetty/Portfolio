25ba898cb379318855e7e6d52b358329be9e2263
Akash Shetty <akashpshetty07@gmail.com>
2021-08-10 09:33:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Engineer", "Developer", "Freelancer", "YouTuber"],
+        strings: ["an Engineer", "a Developer", "a Freelancer", "a YouTuber"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Engineer", "Developer", "Freelancer", "YouTuber"],
+        strings: ["an Engineer", "a Developer", "a Freelancer", "a YouTuber"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
